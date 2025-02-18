9dbe188368f93f25bb979bdd65429e417ec49c67
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-02-18 09:54:53 -0300

    Add files via upload

--- IPido.py
+++ IPido.py
@@ -1,22 +1,38 @@
-print("------------------")
-print("Bem vindo à IPido")
-print("------------------\n")
+import os;
 
-print("1. Cadastrar Restaurante\n")
-print("2. Listar Restaurante\n")
-print("3. Ativar restaurante\n")
-print("4. Sair\n")
+def mostraNome():
+    print("------------------")
+    print("Bem vindo à IPido")
+    print("------------------\n")
+
+def mostraOpcoes():
+    print("1. Cadastrar Restaurante\n")
+    print("2. Listar Restaurante\n")
+    print("3. Ativar restaurante\n")
+    print("4. Sair\n")
 
 opcaoSelecionada = int(input("Escolha uma opção: "))
 print(f'Opção: {opcaoSelecionada}')
+
 def cadastrarRestaurante():
     nomeRestaurante = input("Digite o nome do seu restaurante: ")
     senha = input("Digite a senha do seu restaurante: ")
     return nomeRestaurante, senha
+
 def listarRestaurante():
     print("Listado")
+
 def ativarRestaurante():
     print("Ativado")
+
+def finalizarAPP():
+    os.system("cls")
+    print("Bye")
+
+def main():
+    mostraNome()
+    mostraOpcoes()
+
 
 if opcaoSelecionada == 1:
     cadastrarRestaurante()
@@ -25,6 +41,7 @@ elif opcaoSelecionada == 2:
 elif opcaoSelecionada == 3:
     ativarRestaurante()
 else:
-    print("BYE")
-    exit()
+    finalizarAPP()
 
+if __name__ == '__main__':
+    main()
